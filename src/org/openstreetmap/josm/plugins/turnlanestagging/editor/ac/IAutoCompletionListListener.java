cbf15d06c5304da4d07f53d2f924245dea9a5d22
Vincent Privat <vincent.privat@gmail.com>
2016-09-28 02:05:25 +0200

    remove unused package copied from tageditor plugin

--- src/org/openstreetmap/josm/plugins/turnlanestagging/editor/ac/IAutoCompletionListListener.java
+++
@@ -1,6 +0,0 @@
-// License: GPL. For details, see LICENSE file.
-package org.openstreetmap.josm.plugins.turnlanestagging.editor.ac;
-
-public interface IAutoCompletionListListener {
-    public void autoCompletionItemSelected(String item);
-}
